73192938bd5cb1c0280b9f71516f8b8191c8a0b8
Edgar Flores <e.flores30nov@gmail.com>
2026-03-17 04:14:35 -0600

    deployed system

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,12 @@
-import { LandingPage } from './components/LandingPage'
+import { LandingPage } from "./components/LandingPage";
 
 function App() {
   const handleNavigate = () => {
     // Aquí podrías poner el enlace a tu app real
-    window.location.href = "https://tu-app-principal.vercel.app";
-  }
+    window.location.href = "https://foresight-ten.vercel.app/";
+  };
 
-  return (
-    <LandingPage onNavigateToAuth={handleNavigate} />
-  )
+  return <LandingPage onNavigateToAuth={handleNavigate} />;
 }
 
-export default App
+export default App;
